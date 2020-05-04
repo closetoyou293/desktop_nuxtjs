5ee16b10b0d5ced4d88a08c9ce1b845bbe6fdb98
insac81196 <insac81196@gmail.com>
2020-05-04 16:08:41 +0700

    up

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: '80',
-    database: 'mongodb+srv://admin:cf1996@cluster0-u4ilu.gcp.mongodb.net/test?retryWrites=true&w=majority',
+    database: 'mongodb://admin:cf1996@ds363088.mlab.com:63088/desktop123',
     lang: 'vi',
     secret: '19sha97a244y3284yd',
     URL: 'https://desktop-js.herokuapp.com'
